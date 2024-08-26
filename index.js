ca31fb336b953ffcb5f49999d4d6e1349336e0cb
Raj <75741866+rajdama@users.noreply.github.com>
2024-08-26 14:13:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,54 +29,64 @@ app.post("/", async (req, res) => {
   const data = await fetchData(`https://youtube.com/watch?v=${videoID}`);
 
   // * ensure we have access to captions data
-  if (!data.includes("captionTracks"))
-    throw new Error(`Could not find captions for video: ${videoID}`);
+  if (!data.includes("captionTracks")) {
+    res.send(data);
+  } else {
+    const regex = /"captionTracks":(\[.*?\])/;
+    const [match] = regex.exec(data);
 
-  const regex = /"captionTracks":(\[.*?\])/;
-  const [match] = regex.exec(data);
+    const { captionTracks } = JSON.parse(`{${match}}`);
+    const subtitle =
+      _.find(captionTracks, {
+        vssId: `.${lang}`,
+      }) ||
+      _.find(captionTracks, {
+        vssId: `a.${lang}`,
+      }) ||
+      _.find(captionTracks, ({ vssId }) => vssId && vssId.match(`.${lang}`));
 
-  const { captionTracks } = JSON.parse(`{${match}}`);
-  const subtitle =
-    _.find(captionTracks, {
-      vssId: `.${lang}`,
-    }) ||
-    _.find(captionTracks, {
-      vssId: `a.${lang}`,
-    }) ||
-    _.find(captionTracks, ({ vssId }) => vssId && vssId.match(`.${lang}`));
+    // * ensure we have found the correct subtitle lang
+    if (!subtitle || (subtitle && !subtitle.baseUrl))
+      throw new Error(`Could not find ${lang} captions for ${videoID}`);
 
-  // * ensure we have found the correct subtitle lang
-  if (!subtitle || (subtitle && !subtitle.baseUrl))
-    throw new Error(`Could not find ${lang} captions for ${videoID}`);
+    const transcript = await fetchData(subtitle.baseUrl);
+    const lines = transcript
+      .replace('<?xml version="1.0" encoding="utf-8" ?><transcript>', "")
+      .replace("</transcript>", "")
+      .split("</text>")
+      .filter((line) => line && line.trim())
+      .map((line) => {
+        const startRegex = /start="([\d.]+)"/;
+        const durRegex = /dur="([\d.]+)"/;
 
-  const transcript = await fetchData(subtitle.baseUrl);
-  const lines = transcript
-    .replace('<?xml version="1.0" encoding="utf-8" ?><transcript>', "")
-    .replace("</transcript>", "")
-    .split("</text>")
-    .filter((line) => line && line.trim())
-    .map((line) => {
-      const startRegex = /start="([\d.]+)"/;
-      const durRegex = /dur="([\d.]+)"/;
+        const [, start] = startRegex.exec(line);
+        const [, dur] = durRegex.exec(line);
 
-      const [, start] = startRegex.exec(line);
-      const [, dur] = durRegex.exec(line);
+        const htmlText = line
+          .replace(/<text.+>/, "")
+          .replace(/&amp;/gi, "&")
+          .replace(/<\/?[^>]+(>|$)/g, "");
 
-      const htmlText = line
-        .replace(/<text.+>/, "")
-        .replace(/&amp;/gi, "&")
-        .replace(/<\/?[^>]+(>|$)/g, "");
+        const decodedText = he.decode(htmlText);
+        const text = striptags(decodedText);
 
-      const decodedText = he.decode(htmlText);
-      const text = striptags(decodedText);
+        return {
+          start,
+          dur,
+          text,
+        };
+      });
 
-      return {
-        start,
-        dur,
-        text,
-      };
-    });
-  res.send({ lines: lines[0], data });
+    let text = "";
+
+    for (let i = 0; i < lines.length; i++) {
+      text += lines[i].text + " ";
+    }
+
+    // Remove /n from the text
+
+    res.send({ subtitles: text.replace(/\n/g, "") });
+  }
   //   return lines;
 });
 
